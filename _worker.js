cce0884491eaa0067c872a3efbef06e10bf7eff4
rxsweet <105535100+rxsweet@users.noreply.github.com>
2024-08-20 10:25:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,8 +5,9 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
-	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+	'https://raw.githubusercontent.com/rxsweet/cm_Vmess/main/addresses.txt',
+	//'icook.tw:2053#官方优选域名',
+	//'cloudflare.cfgo.cc#优选官方线路',
 ];
 
 // 设置优选地址api接口
