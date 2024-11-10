2806da93b0ed35d03a930203bdeef65ed8293803
ruanjian1997 <75776665+ruanjian1997@users.noreply.github.com>
2024-11-10 13:34:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,12 +7,13 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 let addresses = [
 	'icook.tw:2053#官方优选域名',
 	'cloudflare.cfgo.cc#优选官方线路',
+	'cmcc.090227.xyz#官方优选域名',
 ];
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
+	'https://raw.githubusercontent.com/ruanjian1997/jiedian/main/443duankou', //可参考内容格式 自行搭建。
+	//'https://raw.githubusercontent.com/ruanjian1997/jiedian/main/IPV6duankou443', //IPv6优选内容格式 自行搭建。
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
